b88d64cc1db0d9d53a14ea8cd306c1562c2b2c0c
dangrenren <441721899@qq.com>
2023-04-25 22:43:03 +0800

    Initial commit

---
+++ src/main/java/com/example/dangrenren_git1/DangrenrenGit1Application.java
@@ -0,0 +1,13 @@
+package com.example.dangrenren_git1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangrenrenGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangrenrenGit1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dangrenren_git1/DangrenrenGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dangrenren_git1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangrenrenGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
